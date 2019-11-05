62a8d462eac89bb03d86823bf8f197c367cb7d5b
Nabh Choudhary <nabh.choudhary4code@gmail.com>
2019-11-06 01:38:50 +0530

    Add files via upload

---
+++ FileHandling/src/com/nabh/test/Trie.java
@@ -0,0 +1,17 @@
+package com.nabh.test;
+
+import java.io.File;
+
+
+public class Trie{
+	public Trie[] children = new Trie[256];
+	public boolean isEndOfWord;
+	public File filePresent;
+	public Trie(){ 
+        isEndOfWord = false; 
+        for (int i = 0; i < 256; i++) 
+            children[i] = null;
+        filePresent=null;
+    } 
+	
+}
